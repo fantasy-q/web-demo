3708cad63252dcc6de53591a38815c048e1fc982
fantasy-q <fantasy-q@users.noreply.github.com>
2021-03-10 21:56:24 +0800

    Update

--- nodejs/beginner-tutorial-mosh/first-app/LoadingModule.js
+++
@@ -1,7 +0,0 @@
-/* Loading a Module */
-
-// const log = require('./logger.js');
-// log('message');
-
-const logger = require('./logger.js');
-logger.log('message');
--- nodejs/beginner-tutorial-mosh/first-app/PathModule.js
+++
@@ -1,6 +0,0 @@
-/* Path Module */
-const path = require('path');
-
-var pathObj = path.parse(__filename);
-
-console.log(pathObj);
